67a6249206751780c2d4286424710e776852dc2d
noname2048 <sungwook.csw@gmail.com>
2021-05-14 16:44:01 +0900

    feat: simple counter with redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-# Vanilla Redux
+import { createStore } from "redux";
 
-Learning Vanilla-Redux and React-Redux
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS"
+
+const countModifier = (count = 0, action) => {
+    switch (action.type) {
+        case ADD:
+            return count + 1
+        case MINUS:
+            return count - 1
+        default:
+            return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({type: ADD});
+
+}
+const handleMinus = () => {
+    countStore.dispatch({type: MINUS})
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
